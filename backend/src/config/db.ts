877030c883fe6bf9879b7f28c49d74f3a674705c
Vivek Rajput <work.tvivekrajput@gmail.com>
2026-06-08 22:20:06 +0530

    render bugssss fixed 3

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -1,3 +1,4 @@
+// import { PrismaClient } from '../../generated/prisma/client'
 import { PrismaClient } from '../../generated/prisma'
 import {PrismaPg} from '@prisma/adapter-pg'
 import { config } from './config'
